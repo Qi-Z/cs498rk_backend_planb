73858881be2f1044a9ef56c2714c913bb0d53e82
Qi Zhang <zhang.qi.js@gmail.com>
2016-12-07 10:35:32 +0000

    more log info

--- app/backend/routes/route.js
+++ app/backend/routes/route.js
@@ -3,10 +3,16 @@ module.exports = function(router, passport) {
     router.post('/signup', passport.authenticate('local-signup'), function(req, res) {
         console.log("req is : =====================================================");
         console.log(req);
+        console.log("Is logged in?? ================================================");
+        console.log(req.isAuthenticated());
         res.status(200).json({ user: req.user });
     });
 
     router.post('/login', passport.authenticate('local-login'), function(req, res) {
+        console.log("req is : =====================================================");
+        console.log(req);
+        console.log("Is logged in?? ================================================");
+        console.log(req.isAuthenticated());
         res.status(200).json({ user: req.user });
     });
 
