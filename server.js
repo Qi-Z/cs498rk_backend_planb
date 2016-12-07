ed5b2a3fe93aa7f6c0b212f53e6bac23271dad50
Qi Zhang <zhang.qi.js@gmail.com>
2016-12-07 10:17:51 +0000

    cors

--- server.js
+++ server.js
@@ -5,7 +5,24 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var app = express();
 
+//Allow CORS so that backend and frontend could pe put on different servers
+var allowCrossDomain = function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+
+
+    next();
+
+};
+app.use(allowCrossDomain);
+
+
 app.use(cookieParser());
+// Use the body-parser package in our application
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 var router = express.Router();
